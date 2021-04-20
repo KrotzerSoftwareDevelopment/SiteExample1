5b1219c5a176e60c78d1a9cd1ca04555c7a59545
deankrotzer <krotzersoftawre@gmail.com>
2021-04-19 21:47:25 -0700

    Updates

---
+++ precache-manifest.b79560ed9b0e0fa840b6421323aa3869.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dcc876da62e275497bb38d00dcb7b7d",
+    "url": "/SiteExample1/index.html"
+  },
+  {
+    "revision": "e5f28f0570325c8e7f84",
+    "url": "/SiteExample1/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "df46b78044f53af0585d",
+    "url": "/SiteExample1/static/css/main.79a9f1d2.chunk.css"
+  },
+  {
+    "revision": "e5f28f0570325c8e7f84",
+    "url": "/SiteExample1/static/js/2.846b39cf.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/SiteExample1/static/js/2.846b39cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df46b78044f53af0585d",
+    "url": "/SiteExample1/static/js/main.fa7fc852.chunk.js"
+  },
+  {
+    "revision": "6eef027f0b42c0805614",
+    "url": "/SiteExample1/static/js/runtime-main.9adcaa44.js"
+  },
+  {
+    "revision": "89ae5b550436c4a1023477b457a109c4",
+    "url": "/SiteExample1/static/media/PortfolioPic.89ae5b55.png"
+  },
+  {
+    "revision": "537d299a2fedf1a7541940cdbfa610d9",
+    "url": "/SiteExample1/static/media/RussoOne-Regular.537d299a.ttf"
+  },
+  {
+    "revision": "a5e1da9b16f99ed2fed4d351515caa24",
+    "url": "/SiteExample1/static/media/bg.a5e1da9b.png"
+  }
+]);
